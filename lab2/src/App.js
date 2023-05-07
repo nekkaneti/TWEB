21b45592d926ee62c9aa1a298841f121ba06cd3e
nekkaneti <neercaterina@gmail.com>
2023-05-07 21:15:33 +0300

    message

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
+  const[toast, settoast]=useState();
+  const onClick = () => {
+    toast.success('Success Notification !', {
+      position: toast.POSITION.TOP_RIGHT
+  });
+    // console.log("Button Pressed");
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +26,16 @@ function App() {
         >
           Learn React Neer Ecaterina CR-203
         </a>
+        <form style={{ display: 'flex', flexDirection: 'column', marginBottom: 10 }}>
+          <label>Login</label>
+          <input></input>
+          <label>Mail</label>
+          <input></input>
+          <label>Password</label>
+          <input></input>
+        </form>
+        <button onClick={onClick}>Submit</button>
+        {/* <ToastContainer/> */}
       </header>
     </div>
   );
